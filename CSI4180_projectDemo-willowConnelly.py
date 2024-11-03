a9b4f406efbfb643007b836d7792b4fc1cbd479c
wiIliu <123427248+wiIliu@users.noreply.github.com>
2024-11-03 01:43:26 -0500

    Update CSI4180_projectDemo-willowConnelly.py

--- CSI4180_projectDemo-willowConnelly.py
+++ CSI4180_projectDemo-willowConnelly.py
@@ -1,3 +1,4 @@
+from sklearn.discriminant_analysis import StandardScaler
 import streamlit as st
 import joblib
 import pandas as pd
@@ -5,15 +6,15 @@ import pandas as pd
 model = joblib.load('xgb.pkl')
 test = pd.read_csv("slectedFeaturesTest.csv")
 
+def prediction(uniqueid):
+    return int(model.predict(StandardScaler().fit_transform([test.iloc[uniqueid,1:]]))[0])
+
 st.title("Vehicle Loan Default Prediction - Willow Connelly")
 
 st.write("This application predicts if a customer is likely to default on their car loan.")
 st.write("0 indicates not likely, 1 indicates likely to default.")
 
 uniqueid = st.slider("Select a unique ID from the test set", min_value=0, max_value=len(test) - 1, value=0)
-
-def prediction(uniqueid):
-    return int(model.predict([test.iloc[uniqueid, 1:]])[0])
 
 if st.button("Predict"):
     pred = prediction(uniqueid)
